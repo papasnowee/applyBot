2e6f718ac5e7819064e752b57eedee22514f0b91
papasnowee <gosusnow@yandex.ru>
2023-05-03 18:56:22 -0700

    Chrome is opened with auto login

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import { Builder, By, WebDriver, WebElement, WebElementPromise } from 'selenium-webdriver'
+import { Builder, By, WebDriver, WebElement } from 'selenium-webdriver'
 import chrome from 'selenium-webdriver/chrome'
 
 import { waitAfterCallback } from './utils'
@@ -14,14 +14,17 @@ async function login(driver: WebDriver) {
 async function openLinkedin() {
   const service = new chrome.ServiceBuilder('C:/Projects/chromedriver_win32/chromedriver.exe')
   const options = new chrome.Options()
-  options.addArguments('start-maximized')
+  options.addArguments(
+    'start-maximized',
+    'user-data-dir=c:/Users/Jilki/AppData/Local/Google/Chrome/User Data/',
+  )
 
   const driver = new Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
     .setChromeService(service)
     .build()
-  await driver.get('https://www.linkedin.com/login')
+  // await driver.get('https://www.linkedin.com/login')
 
   return driver
 }
@@ -39,7 +42,7 @@ async function scrollJobList(driver: WebDriver, element: WebElement) {
 
 const Run = async function () {
   const driver = await openLinkedin()
-  login(driver)
+  // login(driver)
   await driver.sleep(1500)
   await driver.get(
     'https://www.linkedin.com/jobs/search/?currentJobId=3562264659&f_AL=true&f_E=1%2C2%2C3%2C4&f_T=9%2C4677%2C25170%2C100%2C3172%2C7110&f_TPR=r2592000&f_WT=2&geoId=103644278&keywords=NOT%20Nisum%20NOT%20Aquent%20NOT%20diverse%20lynx%20NOT%20info%20way%20solutions%20NOT%20dice%20NOT%20a%20society%20us%20NOT%20orc%20middleware%20test%20company%20NOT%20exatech%20inc%20NOT%20eteam%20NOT%20applab%20NOT%20leadstack%20inc.%20NOT%20arrowcore%20group%20NOT%20compunnel%20inc.%20NOT%20magnus%20technology%20solutions%20NOT%20dynpro%20inc.%20NOT%20shiftcode%20analytics%2C%20inc.%20NOT%20silicon%20spectra%20NOT%20jobot%20NOT%20recruiting%20from%20scratch%20NOT%20underdog.io%20NOT%20binary%20tech%20consulting%20corp%20NOT%20cypress%20hcm%20NOT%20allstem%20connections%20NOT%20motion%20recruitment%20NOT%20maxonic%20NOT%20e-it%20NOT%20photon%20NOT%20icrossing%20NOT%20anew%20recruit%20NOT%20anew%20recruit%20NOT%20Akraya%2C%20Inc.%20%20%20NOT%20MetroStar%20NOT%20Allegis%20Group%20NOT%20Pulivarthi%20Group%20(PG)&location=United%20States&refresh=true&sortBy=R',
